52eec9e46658b0439edd1c52e633175dee05b4f7
andrey <andrey.arakelyan@sentium-consulting.com>
2023-01-11 11:57:54 +0400

    add eslint/prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import { Inter } from '@next/font/google';
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
@@ -16,26 +16,26 @@ export default function Home() {
       </Head>
       <div className={styles.wrapper}>
         <div className={styles.container}>
-            <div className={styles.avatar_container}>
-                <Image className={styles.avatar} width={100} height={100} src='/gofashion-logo.jpg' alt={'logo'} />
-                <div className={styles.title}>Go Fashion Showroom</div>
+          <div className={styles.avatar_container}>
+            <Image className={styles.avatar} width={100} height={100} src="/gofashion-logo.jpg" alt={'logo'} />
+            <div className={styles.title}>Go Fashion Showroom</div>
+          </div>
+          <div className={styles.content_container}>
+            <div className={styles.button}>
+              <div className={styles.text}>Content</div>
             </div>
-            <div className={styles.content_container}>
-                <div className={styles.button}>
-                    <div className={styles.text}>Content</div>
-                </div>
-                <div className={styles.button}>
-                    <div className={styles.text}>Content</div>
-                </div>                <div className={styles.button}>
-                <div className={styles.text}>Content</div>
+            <div className={styles.button}>
+              <div className={styles.text}>Content</div>
             </div>
-                <div className={styles.button}>
-                    <div className={styles.text}>Content</div>
-                </div>
-
+            <div className={styles.button}>
+              <div className={styles.text}>Content</div>
             </div>
+            <div className={styles.button}>
+              <div className={styles.text}>Content</div>
+            </div>
+          </div>
         </div>
       </div>
     </>
-  )
+  );
 }
